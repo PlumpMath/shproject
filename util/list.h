ca0d7a0b78c0a0b692b0ec26e3241248b8c0ef14
Hamish Morrison <hamishm53@gmail.com>
2014-10-21 01:05:42 +0100

    util/list: add some asserts for debugging

--- util/list.h
+++ util/list.h
@@ -29,21 +29,31 @@ static inline void list_init(struct list_node* list) {
 
 
 static inline int list_empty(struct list_node* list) {
+    assert(list->next != NULL && list->prev != NULL);
     return list->next == list;
 }
 
 
+static inline int list_in_list(struct list_node* node) {
+    return node->next != NULL || node->prev != NULL;
+}
+
+
 static inline struct list_node* list_front(struct list_node* list) {
+    assert(list->next != NULL && list->prev != NULL);
     return list->next;
 }
 
 
 static inline struct list_node* list_back(struct list_node* list) {
+    assert(list->next != NULL && list->prev != NULL);
     return list->prev;
 }
 
 
 static inline void list_insert(struct list_node* after, struct list_node* item) {
+    assert(after->next != NULL && after->prev != NULL);
+    assert(after != item);
     item->next = after->next;
     item->prev = after;
     after->next = item;
@@ -52,6 +62,7 @@ static inline void list_insert(struct list_node* after, struct list_node* item) 
 
 
 static inline void list_remove(struct list_node* item) {
+    assert(item->next != NULL && item->prev != NULL);
     item->prev->next = item->next;
     item->next->prev = item->prev;
     item->prev = NULL;
