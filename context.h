e37509c591b29767bc7cf6598fec08582d5392ae
Hamish Morrison <hamishm53@gmail.com>
2015-02-03 19:44:57 +0000

    Context: rewrite context switching routines in assembly
    * The new context_save function allows a two-step sleep, avoiding
      racing with a thread trying to wake a coroutine and restore the
      context.

--- context.h
+++ context.h
@@ -1,35 +1,80 @@
-#ifndef _CORO_H
-#define _CORO_H
+#ifndef _CONTEXT_H
+#define _CONTEXT_H
 
-#include <util/list.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#include <ucontext.h>
+
+#ifdef CONTEXT_GCC_AMD64
+#include <context_gcc_amd64.h>
+#endif
 
 
 struct context {
-    ucontext_t context;
+    struct arch_context arch;
 };
+
+
+extern void _context_create(struct arch_context*, void (*fn)(void*), void* stack_top);
+extern int _context_save(struct arch_context*);
+extern void _context_restore(struct arch_context*);
 
 
 /*
  * Initialise a new coroutine in `coro`. The function `start` will
  * be executed when the coroutine is switched to.
  */
-extern void context_create(struct context* context, void (*start)(void));
+static inline void
+context_create(struct context* context, void (*fn)(void*), size_t stack_size) {
+    void* stack = malloc(stack_size);
+    assert(stack != NULL);
+    _context_create(&context->arch, fn, (void*)((uintptr_t)stack + stack_size));
+}
 
 
 /*
  * Switch to the given coroutine. The function will return when the calling
  * coroutine is switched back to.
  */
-extern void context_switch(struct context* from, struct context* to);
+static inline void
+context_switch(struct context* from, struct context* to) {
+    int switched = _context_save(&from->arch);
+    if (switched == 0) {
+        _context_restore(&to->arch);
+    }
+}
 
 
 /*
  * Get an empty context into which we can store a coroutine state at a later
  * time.
  */
-extern void context_empty(struct context* context);
+static inline void context_empty(struct context* context) { }
+
+
+/*
+ * Save the current execution state into the given context.
+ * Returns false when first called. Returns true when the context is later
+ * restored with context_restore.
+ */
+static inline bool context_save(struct context* context) {
+    return _context_save(&context->arch) != 0;
+}
+
+
+/*
+ * Restores a context previously saved with a call to context_save. Control
+ * flow will be transfered to the original call to context_save, and that
+ * function will return true.
+ */
+static inline void context_restore(struct context* context) __attribute__ ((noreturn));
+
+static inline void context_restore(struct context* context) {
+    _context_restore(&context->arch);
+}
 
 
 #endif
